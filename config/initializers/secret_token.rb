c8d63f0bf537a8096a3c4cd6e0f3031b4fd1d55b
Kim Sarkies <meaculpa123ks@hotmail.com>
2011-04-09 20:55:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exoticdivawear::Application.config.secret_token = 'aa5ea5ffcf020674f7acd2fbb28faaaa81281e1cbd35ffaf787082a7fd4ed08abf13f362bd4d38547ee8d860fc0be0913d1224fdc59168954ce14c2cd4ee1733'
